adf44b2ef0de30feaab4131523a9d9b1d43046dd
Carol Shiers <carol.shiers@gmail.com>
2021-09-29 22:30:43 +1000

    slignemt

--- script.js
+++ script.js
@@ -18,9 +18,6 @@ var questionsArr = [
     {question: "An array can contain:",
     options: ["Objects", "Strings", "Numbers", "All of the Above"],
     answer: "4"},
-    {question: "What is used to contain the OPERATION of a function?",
-    options: ["Square Brackets [ ]", "Curly Brackets { }", "Parentheses ( )", 'Quotation Marks " "'],
-    answer: "2"},
     {question: "A function will run automatically unless set as a variable.",
     options: ["True", "False"],
     answer: "2"},
@@ -28,7 +25,7 @@ var questionsArr = [
     options: ["Confirm", "Alert", "Prompt", "Inform"],
     answer: "1"},
     {question: "What is used after a function name?",
-    options: ["Semi Colon ;", "Square Brackets [ ]", "Parentheses ( )", "Curly Brackets { }"],
+    options: ["Semi Colon ;", "Square Brackets []", "Parentheses ()", "Curly Brackets {}"],
     answer: "3"},
     {question: "This will require the user to input an answer:",
     options: ["Prompt", "Alert", "Confirm", "Window"],
@@ -40,17 +37,11 @@ var questionsArr = [
     options: ["This", "3", "i", "4"],
     answer: "2"},
     {question: "An object is contained in:",
-    options: ["Parentheses ( )", "Colons : :", "Square Brackets [ ]", "Curly Brackets { }"],
+    options: ["Parentheses ()", "Colons : :", "Square Brackets []", "Curly Brackets {}"],
     answer: "4"},
     {question: "A function must be called in order to run.",
     options: ["True", "False"],
-    answer: "1"},
-    {question: "API stands for:",
-    options: ["Automatic Place Integer", "Application Pyramid Itemizer", "Automated Programmer Interface", "Application Programming Interface"],
-    answer: "4"},
-    {question: "DOM stans for:",
-    options: ["Document Override Method", "Document Object Model", "Decrement Object Material", "Do Observe More"],
-    answer: "2"},
+    answer: "1"},       
     {question: "This will increment by one each time:",
     options: ["+-", "+=", "++", "=+"],
     answer: "3"}
@@ -97,8 +88,8 @@ var startQuiz = function() {
     //start timer
     countdown();
     // style quiz display section elements
-    questionEl.setAttribute("style", "text-align: left;");
-    messageEl.setAttribute("style", "margin-left: 25px; width: fit-content;");
+    questionEl.setAttribute("style", "text-align: centre;");
+    messageEl.setAttribute("style", " width: fit-content;");
     // remove start button
     startBtn.remove();
 
