4d1fb74a4bad469bac2755c10f6e82f4bdfa0a13
Carol Shiers <carol.shiers@gmail.com>
2021-09-29 21:27:53 +1000

    highscores updated

--- highscore.js
+++ highscore.js
@@ -1,28 +1,55 @@
-var clearBtn = document.getElementById("clear");
+// get ul element
+var ulEl = document.querySelector("#scores");
+// get return button
+var returnEl = document.querySelector("#return-button")
+// get clear scores button
+var clearEl = document.querySelector("#clear-button");
 
-// Clear High Scores
-clearBtn.addEventListener("click", function() {
-    localStorage.clear();
-})
+// global variables
+var highScores = [];
 
-var finalScore = {
-  contactInfo: 'Briana',
-  timeleft: "30"
-}
-var finalScore = [];
-
-//Load Scores - not working
+// function to get and display high scores from localStorage
 var loadScores = function() {
-  var finalScore = localStorage.getItem("contactInfo, timeLeft");
-  
-      if (!finalScore) {
-          return false;
-      }
-    
-       for (var i = 0; i < finalScore.length; i++) {
-        var finalScore = finalScore[i];
+    highScores = localStorage.getItem("scores");
+    // check if scores is null/falsy
+    if(!highScores) {
+        highScores = [];
+        return false;
+    }
+    // convert highScores from stringified format to an array of objects
+    highScores = JSON.parse(highScores);
+    console.log(highScores);
+    // iterate through highScores array and add a list item to the page for each score
+    for(var i = 0; i < highScores.length; i++) {
         var listItemEl = document.createElement("li");
-        listItemEl.textContent = finalScore;
-      }    
-    finalScore.push(finalScore)   
+        listItemEl.className = "listed-score";
+        var nameEl = document.createElement("div");
+        nameEl.className = "score-info";
+        nameEl.textContent = `Initials: ${highScores[i].initials}`;
+        listItemEl.appendChild(nameEl);
+        var scoreEl = document.createElement("div");
+        scoreEl.className = "score-info";
+        scoreEl.textContent = ` Score: ${highScores[i].score}`;
+        listItemEl.appendChild(scoreEl);
+        ulEl.appendChild(listItemEl);
+    }
 };
+
+// function to return to index.html
+var goToIndex = function() {
+    location.replace("https://caz1502.github.io/Code-Quiz/");
+};
+
+// function to clear the high scores stored in localStorage
+var clearScores = function() {
+    highScores = [];
+    localStorage.setItem("scores", highScores);
+    location.reload();
+};
+
+// event listener for return button
+returnEl.addEventListener("click", goToIndex);
+// event listener for clear scores button
+clearEl.addEventListener("click", clearScores);
+
+loadScores();
