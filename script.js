a70d433b847ae3dae9c0b447b66cad256dd223fb
Carol Shiers <carol.shiers@gmail.com>
2021-09-30 12:51:02 +1000

    updated music function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// audio function
+const button = document.querySelector("#start-quiz");
+const icon = document.querySelector("#start-quiz > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
+});
 
 // get elements for DOM manipulation
 // timer
